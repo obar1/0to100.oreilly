c27818142c4bf8a56e0a4e32105bc4a2f7325768
obar1 <387386+obar1@users.noreply.github.com>
2024-03-23 12:20:50 +0100

    wip

--- ignore-drobox-folders.ps1
+++ ignore-drobox-folders.ps1
@@ -1,3 +1,3 @@
  Set-Content -Path '.git' -Stream com.dropbox.ignored -Value 1
  Set-Content -Path 'zero*' -Stream com.dropbox.ignored -Value 1
- 
+  Set-Content -Path '978*' -Stream com.dropbox.ignored -Value 0
